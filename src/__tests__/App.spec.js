17eb4a4c2ce19c1e09c49617f3ecdb859623ed9b
João Paulo Lima <jptick@gmail.com>
2020-09-15 21:51:41 -0300

    Update React-Native Functions Handler

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -9,78 +9,77 @@ import App from "../App";
 import { render, fireEvent, act } from "@testing-library/react-native";
 
 const wait = (amount = 0) => {
-  return new Promise((resolve) => setTimeout(resolve, amount));
+    return new Promise((resolve) => setTimeout(resolve, amount));
 };
 
-const actWait = async (amount = 0) => {
-  await act(async () => {
-    await wait(amount);
-  });
+const actWait = async(amount = 0) => {
+    await act(async() => {
+        await wait(amount);
+    });
 };
 
 const repositoryId = 'd6e43105-a559-45b7-8fd7-53416b415741';
 
-apiMock.onGet("repositories").reply(200, [
-  {
+apiMock.onGet("repositories").reply(200, [{
     id: repositoryId,
     title: "Desafio React Native",
     url: "https://github.com/josepholiveira",
     techs: ["React Native", "Node.js"],
     likes: 0,
-  },
-]);
+}, ]);
 
 jest.mock(
-  "react-native/Libraries/Components/Touchable/TouchableOpacity.js",
-  () => {
-    const { TouchableHighlight } = require("react-native");
+    "react-native/Libraries/Components/Touchable/TouchableOpacity.js",
+    () => {
+        const { TouchableHighlight } = require("react-native");
 
-    const MockTouchable = (props) => {
-      return <TouchableHighlight {...props} />;
-    };
+        const MockTouchable = (props) => {
+            return <TouchableHighlight {...props }
+            />;
+        };
 
-    MockTouchable.displayName = "TouchableOpacity";
+        MockTouchable.displayName = "TouchableOpacity";
 
-    return MockTouchable;
-  }
+        return MockTouchable;
+    }
 );
 
 describe("Likes", () => {
-  it("should add a like to the like counter of the repository", async () => {
-    const { getByTestId } = render(<App />);
+    it("should add a like to the like counter of the repository", async() => {
+        const { getByTestId } = render( < App / > );
 
-    apiMock
-      .onPost(`repositories/${repositoryId}/like`)
-      .reply(200, {
-        id: repositoryId,
-        title: "Desafio React Native",
-        url: "https://github.com/josepholiveira",
-        techs: ["React Native", "Node.js"],
-        likes: 1,
-      });
+        apiMock
+            .onPost(`repositories/${repositoryId}/like`)
+            .reply(200, {
+                id: repositoryId,
+                title: "Desafio React Native",
+                url: "https://github.com/josepholiveira",
+                techs: ["React Native", "Node.js"],
+                likes: 1,
+            });
 
-    await actWait();
+        await actWait();
 
-    fireEvent.press(getByTestId(`like-button-${repositoryId}`));
+        fireEvent.press(getByTestId(`like-button-${repositoryId}`));
 
-    await actWait();
+        await actWait();
 
-    expect(getByTestId(`repository-likes-${repositoryId}`)).toHaveTextContent("1 curtida");
+        expect(getByTestId(`repository-likes-${repositoryId}`)).toHaveTextContent("1 curtida");
 
-    apiMock
-      .onPost(`repositories/${repositoryId}/like`)
-      .reply(200, {
-        id: repositoryId,
-        title: "Desafio React Native",
-        url: "https://github.com/josepholiveira",
-        techs: ["React Native", "Node.js"],
-        likes: 2,
-      });
+        apiMock
+            .onPost(`repositories/${repositoryId}/like`)
+            .reply(200, {
+                id: repositoryId,
+                title: "Desafio React Native",
+                url: "https://github.com/josepholiveira",
+                techs: ["React Native", "Node.js"],
+                likes: 2,
+            });
 
-    fireEvent.press(getByTestId(`like-button-${repositoryId}`));
+        fireEvent.press(getByTestId(`like-button-${repositoryId}`));
 
-    await actWait();
+        await actWait();
 
-    expect(getByTestId(`repository-likes-${repositoryId}`)).toHaveTextContent("2 curtidas");
-  });
+        expect(getByTestId(`repository-likes-${repositoryId}`)).toHaveTextContent("2 curtidas");
+    });
 });
